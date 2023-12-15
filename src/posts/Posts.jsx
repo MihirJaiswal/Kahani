105305a739f05e531ba93c261bdabe3c90bd40b4
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-12-15 10:20:52 +0530

    added sidebar

---
+++ src/posts/Posts.jsx
@@ -0,0 +1,9 @@
+import "./post.scss"
+
+export default function Posts() {
+  return (
+    <div className="posts">
+      post
+    </div>
+  )
+}
--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -3,12 +3,32 @@ import "./sidebar.scss"
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sliderbarItem">
-        <span className="sliderbarIcon fa-solid fa-house">About me</span>
-        <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" />
+      <div className="sidebarItem">
+        <span className="sidebarTitle">About me</span>
+        <img src="" alt="" />
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem fuga aperiam voluptate rerum debitis exercitationem.
         </p>
+      </div>
+      <div className="sidebarItem">
+      <span className="sidebarTitle">Genre</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Horror</li>
+          <li className="sidebarListItem">Romance</li>
+          <li className="sidebarListItem">Adventure</li>
+          <li className="sidebarListItem">Action</li>
+          <li className="sidebarListItem">Drama</li>
+          <li className="sidebarListItem">Comedy</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+      <span className="sidebarTitle">FOLLOWERS</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+          <i className="sidebarIcon fa-brands fa-square-x-twitter"></i>
+          <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+          <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+        </div>
       </div>
     </div>
   )
