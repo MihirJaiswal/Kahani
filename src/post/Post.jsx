af58e8eeabce23fb7d501137e0277039c2145a1a
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-12-15 13:01:43 +0530

    some changes

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -8,7 +8,7 @@ export default function Post() {
 
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
+          <span className="postCat">Social</span>
           <span className="postCat">Life</span>
         </div>
         <span className="postTitle">Lorem ipsum dolor sit amet</span>
