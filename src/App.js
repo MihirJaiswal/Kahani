ebf1074e3bc91870fd80a6ba33654dfac4f6c804
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-12-16 19:46:34 +0530

    added creating post page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Home from "./pages/home/Home";
 import Topbar from "./components/top-bar/Topbar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 
 function App() {
   return (
   <>
    <Topbar />
-   <Single />
+   <Write />
    
   </>
   );
