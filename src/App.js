3597a905956646c9381e1cc64f46047bcee0370c
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-12-17 08:56:00 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -6,14 +6,40 @@ import Setting from "./pages/setting/Setting";
 import Sidebar from "./components/sidebar/Sidebar";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 
 function App() {
   return (
-  <>
+  <Router>
    <Topbar />
-   <Register />
-   </>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/register">
+        <Register />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/write">
+        <Write />
+      </Route>
+      <Route path="/setting">
+        <Setting />
+      </Route>
+      <Route path="/post/:postId">
+        <Single />
+      </Route>
+    </Switch>
+   </Router>
   );
 }
 
